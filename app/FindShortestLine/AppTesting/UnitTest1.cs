3b8668e9f8b5fd1d038a00577b18679f63fc42f4
Ksenia <stu-ipsp122@vlsu.ru>
2025-12-26 11:45:24 +0300

    Add tests

---
+++ app/FindShortestLine/AppTesting/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using FindShortestLine.Classes;
+
+namespace AppTesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            // Arrange
+            Graph graph = new Graph(3);
+            double[,] m = {
+            {0, 4, 2} ,  
+            {4, 0, 1} ,  
+            {2, 1, 0}  
+                };
+            graph.incidenceMatrix = m;
+
+            // Act
+            double result = graph.ComparePoints(0, 1);
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
--- app/FindShortestLine/FindShortestLine/obj/Debug/net10.0/FindShortestLine.AssemblyInfo.cs
+++ app/FindShortestLine/FindShortestLine/obj/Debug/net10.0/FindShortestLine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FindShortestLine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82b65e7acb830c103d2f3e43f85b366484c13c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df485b831c248f86ecd44682af0c757758a602d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FindShortestLine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FindShortestLine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
